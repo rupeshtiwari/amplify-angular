ac7057899ca9ef0d6c09b962509eab4d0c659147
Rupesh Kumar Tiwari <roopkt@gmail.com>
2019-07-04 09:09:22 -0400

    created angular app and auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { SecureComponent } from './secure/secure.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AuthComponent, HomeComponent, SecureComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
